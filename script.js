d9066763c17e08317c832822eeb16aaedc6b796b
Gustavo De Pieri <gustdepieri@gmail.com>
2025-02-03 20:53:46 -0300

    nada demais

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
 function salvarRegistro() {
     const entrada = document.getElementById("entrada").value;
     const almocoEntrada = document.getElementById("almocoEntrada").value;
