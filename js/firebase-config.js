8141b7b394ef8bba7454fb278ec9911f927803e3
Gustavo De Pieri <gustdepieri@gmail.com>
2025-03-05 10:34:20 -0300

    update firebase

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,14 +1,14 @@
  
  // Substitua estas configurações pelas suas do projeto Firebase
  const firebaseConfig = {
-    apiKey: "AIzaSyBOJitMoSPQtWiYIXsy1T4v814tRLhnS-M",
-    authDomain: "aplicativo-registra-horas.firebaseapp.com",
-    projectId: "aplicativo-registra-horas",
-    storageBucket: "aplicativo-registra-horas.firebasestorage.app",
-    messagingSenderId: "509122969210",
-    appId: "1:509122969210:web:273660fcb9fd30df04c5c3",
-    measurementId: "G-HL98RX8XBG"
-  };
+  apiKey: "AIzaSyBOJitMoSPQtWiYIXsy1T4v814tRLhnS-M",
+  authDomain: "aplicativo-registra-horas.firebaseapp.com",
+  projectId: "aplicativo-registra-horas",
+  storageBucket: "aplicativo-registra-horas.firebasestorage.app",
+  messagingSenderId: "509122969210",
+  appId: "1:509122969210:web:273660fcb9fd30df04c5c3",
+  measurementId: "G-HL98RX8XBG"
+};
   
 
 // Initialize Firebase
